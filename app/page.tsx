ab13c8df6f0db2224beae3e26e1ebc02e9827578
Changlin <CarberryChai@gmail.com>
2023-12-25 09:11:44 +0800

    Nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className='flex gap-5'>
-      hello world!
+      <Button>click me</Button>
       <Link href='/card'>Card</Link>
     </main>
   )
